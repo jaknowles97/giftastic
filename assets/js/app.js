c3d275375a46929622917630590ee016ba0316da
Andrew <jaknowles1997@gmail.com>
2018-03-09 05:22:55 -0800

    changed queryUrl from http to https. github pages would not load gifs unless secure.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,9 @@
 var currentSearch;
-var apiKey = "&api_key=KC2URc4m6dX6cSEp1G3hlItyPX612BZS&limit=50";
-var queryURL = "http://api.giphy.com/v1/gifs/search?q="
+var apiKey = "&api_key=KC2URc4m6dX6cSEp1G3hlItyPX612BZS&limit=50&q=";
+var queryURL = "https://api.giphy.com/v1/gifs/search?" + apiKey; 
 var limit;
 var sessionBtns;
-var defaultBtns = ["deadpool", "Ozzy", "neko"]
+var defaultBtns = ["deadpool", "burp", "barf"]
 
 var makeReq = function(queryURL, limit) {
     $.ajax({
@@ -12,7 +12,9 @@ var makeReq = function(queryURL, limit) {
     }).then(function(giphy) {
         for(var j=0; j < limit; j++) {
             var gifSrc = giphy.data[j].images.downsized.url;
-            gif = $("<img src=" + gifSrc + ">");
+            var still = giphy.data[j].images.downsized_still.url;
+
+            gif = $("<img src='" + still + "'data-still='"+still + "'data-animate='" + gifSrc + "'data-state='still' class='gif'>");
             $("#display-gifs").append(gif)
         }
         console.log(queryURL);
@@ -28,7 +30,7 @@ var renderBtns = function() {
       }
     
     for(var i=0; i < sessionBtns.length; i++) {
-        makeReq(queryURL+sessionBtns[i]+apiKey);
+        makeReq(queryURL+sessionBtns[i]);
         var clientBtn = $("<button class='btn addedBtn m-1' data-name=" + sessionBtns[i] + ">" + sessionBtns[i] + "</button>");
         $("#added-btns").append(clientBtn);
     }
@@ -39,18 +41,32 @@ var renderBtns = function() {
     }
 }
 
-$("#submitBtn").on("click", function() {
+$("#submitBtn").on("click", function(event) {
+    event.preventDefault();
     Currentsearch = $("#user-search").val().trim();
     sessionBtns.push(Currentsearch);
     sessionStorage.setItem("sessionBtns", JSON.stringify( sessionBtns));
     var addedBtn = $("<button class='btn addedBtn' data-name=" + Currentsearch + ">" + Currentsearch + "</button>")
     $("#added-btns").append(addedBtn);
-    makeReq(queryURL+Currentsearch+apiKey,1);
+    makeReq(queryURL+Currentsearch, 10);
 });
 
 $(document).on("click",".addedBtn", function() {
     var name = $(this).data('name');
-    makeReq(queryURL+name+apiKey, 1);
+    makeReq(queryURL+name, 10);
 })
+
+$(document).on("click", ".gif", function() {
+
+    var state = $(this).attr("data-state");
+
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
 
 renderBtns();
